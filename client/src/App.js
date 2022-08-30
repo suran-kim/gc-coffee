d19407e14a6ad597ec037c4c5bfeddbdb70a68b1
moonstal1506 <moonstal1506@gmail.com>
2022-08-30 11:11:23 +0900

    feat: 상품 추가

--- client/src/App.js
+++ client/src/App.js
@@ -27,6 +27,24 @@ function App() {
     },
   ]); //기본상태
   const [items, setItems] = useState([]);
+  const handleAddClicked = (id) => {
+    //같은 아이디 찾기
+    const product = products.find((v) => v.id === id);
+    const found = items.find((v) => v.id === id);
+    const updatedItems =
+      //상품확인...모든속성 복사, 개수 +1/없으면 기존 아이템 반환
+      //기존것 복사 + 새로운 상품 추가
+      found
+        ? items.map((v) => (v.id === id ? { ...v, count: v.count + 1 } : v))
+        : [
+            ...items,
+            {
+              ...product,
+              count: 1,
+            },
+          ];
+    setItems(updatedItems);
+  };
 
   return (
     <body className="container-fluid">
@@ -36,7 +54,7 @@ function App() {
       <div className="card">
         <div className="row">
           <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
-            <ProductList products={products} />
+            <ProductList products={products} onAddClick={handleAddClicked} />
           </div>
           <div className="col-md-4 summary p-4">
             <Summary items={items} />
--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,23 +1,20 @@
 import React from "react";
-import {Product} from "./Product";
+import { Product } from "./Product";
 
-export function ProductList({products = []}) {
-    return (
-        <React.Fragment>
-            <h5 className="flex-grow-0">
-                <b>상품 목록</b>
-            </h5>
-            <ul className="list-group products">
-                {products.map((v) => (
-                    <li key={v.id} className="list-group-item d-flex mt-3">
-                        <Product
-                            productName={v.productName}
-                            category={v.category}
-                            price={v.price}
-                        />
-                    </li>
-                ))}
-            </ul>
-        </React.Fragment>
-    );
+export function ProductList({ products = [], onAddClick }) {
+  return (
+    <React.Fragment>
+      <h5 className="flex-grow-0">
+        <b>상품 목록</b>
+      </h5>
+      <ul className="list-group products">
+        {products.map((v) => (
+          <li key={v.id} className="list-group-item d-flex mt-3">
+            {/* 프로퍼티같으면 ...표현(스프레드연산자) */}
+            <Product {...v} onAddClick={onAddClick} />
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 }
