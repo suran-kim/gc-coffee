225d454211bc137c88b30506a1a1d96a84e4da94
moonstal1506 <moonstal1506@gmail.com>
2022-08-30 10:39:21 +0900

    refactor: 컴포넌트로 분리

--- client/src/App.js
+++ client/src/App.js
@@ -1,84 +1,49 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import React, { useState } from "react";
+import { ProductList } from "./components/ProductList";
+import { Summary } from "./components/Summary";
 
 function App() {
+  // 상태에 접근할 수 있는 변수명, 상태값 바꾸는 함수
+  const [products, setProducts] = useState([
+    {
+      id: "uuid-1",
+      productName: "콜롬비아커피1",
+      category: "커피빈",
+      price: 3000,
+    },
+    {
+      id: "uuid-2",
+      productName: "콜롬비아커피2",
+      category: "커피빈",
+      price: 3000,
+    },
+    {
+      id: "uuid-3",
+      productName: "콜롬비아커피3",
+      category: "커피빈",
+      price: 3000,
+    },
+  ]); //기본상태
+  const [items, setItems] = useState([]);
+
   return (
-      <body className="container-fluid">
+    <body className="container-fluid">
       <div className="row justify-content-center m-4">
         <h1 className="text-center">Grids & Circle</h1>
       </div>
       <div className="card">
         <div className="row">
           <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
-            <h5 className="flex-grow-0"><b>상품 목록</b></h5>
-            <ul className="list-group products">
-              <li className="list-group-item d-flex mt-3">
-                <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/></div>
-                <div className="col">
-                  <div className="row text-muted">커피콩</div>
-                  <div className="row">Columbia Nariñó</div>
-                </div>
-                <div className="col text-center price">5000원</div>
-                <div className="col text-end action"><a className="btn btn-small btn-outline-dark" href="">추가</a></div>
-              </li>
-              <li className="list-group-item d-flex mt-2">
-                <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/></div>
-                <div className="col">
-                  <div className="row text-muted">커피콩</div>
-                  <div className="row">Columbia Nariñó</div>
-                </div>
-                <div className="col text-center price">5000원</div>
-                <div className="col text-end action"><a className="btn btn-small btn-outline-dark" href="">추가</a></div>
-              </li>
-              <li className="list-group-item d-flex mt-2">
-                <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/></div>
-                <div className="col">
-                  <div className="row text-muted">커피콩</div>
-                  <div className="row">Columbia Nariñó</div>
-                </div>
-                <div className="col text-center price">5000원</div>
-                <div className="col text-end action"><a className="btn btn-small btn-outline-dark" href="">추가</a></div>
-              </li>
-            </ul>
+            <ProductList products={products} />
           </div>
           <div className="col-md-4 summary p-4">
-            <div>
-              <h5 className="m-0 p-0"><b>Summary</b></h5>
-            </div>
-            <hr/>
-              <div className="row">
-                <h6 className="p-0">Columbia Nariñó <span className="badge bg-dark text-">2개</span></h6>
-              </div>
-              <div className="row">
-                <h6 className="p-0">Brazil Serra Do Caparaó <span className="badge bg-dark">2개</span></h6>
-              </div>
-              <div className="row">
-                <h6 className="p-0">Columbia Nariñó <span className="badge bg-dark">2개</span></h6>
-              </div>
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">이메일</label>
-                  <input type="email" className="form-control mb-1" id="email"/>
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="address" className="form-label">주소</label>
-                  <input type="text" className="form-control mb-1" id="address"/>
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="postcode" className="form-label">우편번호</label>
-                  <input type="text" className="form-control" id="postcode"/>
-                </div>
-                <div>당일 오후 2시 이후의 주문은 다음날 배송을 시작합니다.</div>
-              </form>
-              <div className="row pt-2 pb-2 border-top">
-                <h5 className="col">총금액</h5>
-                <h5 className="col text-end">15000원</h5>
-              </div>
-              <button className="btn btn-dark col-12">결제하기</button>
+            <Summary items={items} />
           </div>
         </div>
       </div>
-      </body>
+    </body>
   );
 }
 
