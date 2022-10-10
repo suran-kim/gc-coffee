e31a1db2ed95db003caafaa0177d5c7d4d3e3594
moonstal1506 <moonstal1506@gmail.com>
2022-10-10 22:29:59 +0900

    chore: mariaDB, 자바 11 버전으로 바꿈

--- server/src/main/java/com/example/gccoffee/controller/CreateOrderRequest.java
+++ server/src/main/java/com/example/gccoffee/controller/CreateOrderRequest.java
@@ -3,8 +3,47 @@ package com.example.gccoffee.controller;
 import com.example.gccoffee.model.OrderItem;
 
 import java.util.List;
+import java.util.Objects;
 
-public record CreateOrderRequest(
-        String email, String address, String postcode, List<OrderItem> orderItems
-) {
+public final class CreateOrderRequest {
+    private final String email;
+    private final String address;
+    private final String postcode;
+    private final List<OrderItem> orderItems;
+
+    CreateOrderRequest(String email, String address, String postcode, List<OrderItem> orderItems) {
+        this.email = email;
+        this.address = address;
+        this.postcode = postcode;
+        this.orderItems = orderItems;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public String address() {
+        return address;
+    }
+
+    public String postcode() {
+        return postcode;
+    }
+
+    public List<OrderItem> orderItems() {
+        return orderItems;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreateOrderRequest that = (CreateOrderRequest) o;
+        return email.equals(that.email) && address.equals(that.address) && postcode.equals(that.postcode) && orderItems.equals(that.orderItems);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, address, postcode, orderItems);
+    }
 }
--- server/src/main/java/com/example/gccoffee/model/OrderItem.java
+++ server/src/main/java/com/example/gccoffee/model/OrderItem.java
@@ -2,5 +2,59 @@ package com.example.gccoffee.model;
 
 import java.util.Objects;
 import java.util.UUID;
-public record OrderItem(UUID productId, Category category, long price, int quantity) {
-        }
+
+public final class OrderItem {
+    private final UUID productId;
+    private final Category category;
+    private final long price;
+    private final int quantity;
+
+    OrderItem(UUID productId, Category category, long price, int quantity) {
+        this.productId = productId;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public UUID productId() {
+        return productId;
+    }
+
+    public Category category() {
+        return category;
+    }
+
+    public long price() {
+        return price;
+    }
+
+    public int quantity() {
+        return quantity;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (OrderItem) obj;
+        return Objects.equals(this.productId, that.productId) &&
+                Objects.equals(this.category, that.category) &&
+                this.price == that.price &&
+                this.quantity == that.quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, category, price, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem[" +
+                "productId=" + productId + ", " +
+                "category=" + category + ", " +
+                "price=" + price + ", " +
+                "quantity=" + quantity + ']';
+    }
+
+}
